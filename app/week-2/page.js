ece07cbaa030d44fee71da7c4fd4f783e217cedd
Taze-Priizer <diem.salaysay@edu.sait.ca>
2024-09-13 14:15:20 -0600

    Created student-info file
    Finished putting in my name and my GitHub repository.

--- app/week-2/page.js
+++ app/week-2/page.js
@@ -1,7 +1,8 @@
-
+import StudentInfo from "./student-info";
 
 export default function Page() {
     <main>
         <h1>Shopping List</h1>
+        <StudentInfo/>
     </main>
 }
--- app/week-2/student-info.js
+++ app/week-2/student-info.js
@@ -1,7 +1,9 @@
+import Link from "next/link";
 
 
 export default function StudentInfo() {
     <main>
-        
+        <h2>Diem Salaysay</h2>
+        <Link href="https://github.com/Taze-Priizer/cprg306-assignments" className="underline text-emerald-500 hover:text-emerald-900">GitHub Repository</Link>
     </main>
 }
