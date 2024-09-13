f6d32d0365934a1d324552f43adcf9beb369e55c
Taze-Priizer <diem.salaysay@edu.sait.ca>
2024-09-13 13:50:58 -0600

    Created student-info file and created week 2 page
    edit to the page file from week 2 and added student info file from week 2

--- app/week-2/page.js
+++ app/week-2/page.js
@@ -2,6 +2,6 @@
 
 export default function Page() {
     <main>
-        
+        <h1>Shopping List</h1>
     </main>
 }
---
+++ app/week-2/student-info.js
@@ -0,0 +1,7 @@
+
+
+export default function StudentInfo() {
+    <main>
+        
+    </main>
+}
