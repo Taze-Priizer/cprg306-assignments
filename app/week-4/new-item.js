edcfcbd16c5203551389143229174e6bddfd90f8
Taze-Priizer <diem.salaysay@edu.sait.ca>
2024-10-04 14:33:00 -0600

    Created useState, as well as increment and decrement constants
    NewItem is linked to page.js file

--- app/week-4/new-item.js
+++ app/week-4/new-item.js
@@ -2,9 +2,22 @@
 
 import { useState } from "react";
 
-export default function(){
-    return{
+export default function NewItem(){
 
+    const [quantity, setQuantity] = useState(1);
+
+    const increment = () => {
+        
     }
+
+    return (
+        <form>
+            <div>
+                <span>{quantity}</span>
+                <button>Increment</button>
+                <button>Decrement</button>
+            </div>
+        </form>
+    );
 
 }
