89a796e3130bcc5a00fafbf19098295faff82e85
Taze-Priizer <diem.salaysay@edu.sait.ca>
2024-10-07 15:31:50 -0600

    Increment and decrement buttons have some styling, as well as button disable
    Button disable is not working yet, will have to fix it soon

--- app/week-4/new-item.js
+++ app/week-4/new-item.js
@@ -6,26 +6,22 @@ export default function NewItem(){
 
     const [quantity, setQuantity] = useState(1);
 
-    const increment = () => {
-        setQuantity(quantity + 1)
-        if( quantity >= 20){
-            setQuantity(20)
-        }
-    }
+    let btnDisabled = false;
 
-    const decrement = () => {
-        setQuantity(quantity - 1)
-        if( quantity <= 1){
-            setQuantity(1)
+    const increment = () => {
+        if( quantity < 20 ){setQuantity(quantity + 1)
         }
-    }
+        if( quantity >= 20 ){
+            btnDisabled = true;}
+    };
+
+    let btnStyles = "bg-blue-600 text-white rounded py-2 px-4 mt-5 mr-3 hover:bg-blue-400 active:bg-yellow-600 disabled:bg-gray-500";
 
     return (
-
             <div className="bg-orange-300 p-5">
-                <span className="text-2xl lg">{quantity}</span>
-                <button onClick={ increment }>Increase</button>
-                <button onClick={ decrement }>Decrease</button>
+                <p className="text-2xl lg">{quantity}</p>
+                <button className={btnStyles} onClick={ increment } disabled={btnDisabled}>Increase</button>
+
             </div>
 
     );
--- app/week-4/page.js
+++ app/week-4/page.js
@@ -1,6 +1,12 @@
 import NewItem from "./new-item";
 
-export default function Page(){
+export default function Page(quantity, increment, decrement){
+
+    let btnDisabled = false;
+    if( quantity >= 20 ){
+        increment.btnDisabled = true;
+    }
+    if( quantity )
 
     return(
     <main className="p-5">
