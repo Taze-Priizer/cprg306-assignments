5e00ea61f6ba7476a682e999559a7804070d2004
Taze-Priizer <diem.salaysay@edu.sait.ca>
2024-10-21 12:46:25 -0600

    Gave 2 button functions with conditional statements to work accordingly
    Increase and decrease button has their own separate functions so it works properly once it reaches a certain limit for their side

--- app/week-4/new-item.js
+++ app/week-4/new-item.js
@@ -6,28 +6,30 @@ export default function NewItem(){
 
     const [quantity, setQuantity] = useState(1);
 
-    let btnDisabled = false;
+
+    let incBtnDisabled = quantity >= 20 ? true : false;
+    let decBtnDisabled = quantity <= 1 ? true : false;
+
 
     const increment = () => {
-        if( quantity < 20 ){setQuantity(quantity + 1)
-            btnDisabled = false;
+        if( quantity < 20 ){
+            setQuantity(quantity + 1);
         }
-        else if( quantity >= 20 ){
-            btnDisabled = true;}
     };
 
     const decrement = () => {
-        if( quantity > 1 ){setQuantity(quantity - 1)}
-        else if (quantity <= 0 ){btnDisabled = true;}
+        if( quantity > 1 ){
+            setQuantity(quantity - 1);
+        }
     };
 
-    let btnStyles = "bg-blue-600 text-white rounded py-2 px-4 mt-5 mr-3 hover:bg-blue-400 active:bg-yellow-600 disabled:bg-gray-500";
+    let btnStyles = "bg-blue-600 text-white rounded py-2 px-4 mt-5 mr-3 hover:bg-blue-400 active:bg-yellow-600 disabled:bg-red-400";
 
     return (
             <div className="bg-orange-300 p-5">
                 <p className="text-2xl lg">{quantity}</p>
-                <button className={btnStyles} onClick={ increment } disabled={btnDisabled}>Increase</button>
-                <button className={btnStyles} onClick={ decrement } disabled={btnDisabled}>Decrease</button>
+                <button className={btnStyles} onClick={ increment } disabled={incBtnDisabled}>Increase</button>
+                <button className={btnStyles} onClick={ decrement } disabled={decBtnDisabled}>Decrease</button>
             </div>
 
     );
--- app/week-4/page.js
+++ app/week-4/page.js
@@ -1,12 +1,6 @@
 import NewItem from "./new-item";
 
-export default function Page(quantity, increment, decrement){
-
-    let btnDisabled = false;
-    if( quantity >= 20 ){
-        increment.btnDisabled = true;
-    }
-    if( quantity )
+export default function Page(){
 
     return(
     <main className="p-5">
