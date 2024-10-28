3a1840c6a9d2df1e01c29e2254d6ce4c3939fcf7
Taze-Priizer <diem.salaysay@edu.sait.ca>
2024-10-27 22:41:02 -0600

    added functions and skeleton form for the options

--- app/week-5/new-item.js
+++ app/week-5/new-item.js
@@ -24,15 +24,54 @@ export default function NewItem(){
             setQuantity(quantity - 1);
         }
     };
+    
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.dir(event);
+
+        const item = {
+            name,
+            category,
+            quantity
+        }
+        
+        console.log(item);
+        alert("Added name: " + name + " category: " + category + " quantity: " + quantity);
+        console.log(name, category, quantity);
+        reset();
+    };
+
+    const reset = () => {
+        setName("");
+        setCategory("produce");
+        setQuantity(1);
+    }
+
+    const categoryOptions = ["Produce", "Dairy", "Bakery", "Meat", "Frozen Foods", "Canned Goods", "Dry Goods", "Beverages", "Snacks", "Household", "Other"];
 
     let btnStyles = "bg-blue-600 text-white rounded py-2 px-4 mt-5 mr-3 hover:bg-blue-400 active:bg-yellow-600 disabled:bg-red-400";
 
     return (
+        <form onSubmit={handleSubmit}>
+            <div>
+                <label>Name:</label>
+                <input type="text" />
+            </div>
             <div className="bg-orange-300 p-5">
                 <p className="text-2xl lg">{quantity}</p>
                 <button className={btnStyles} onClick={ increment } disabled={incBtnDisabled}>Increase</button>
                 <button className={btnStyles} onClick={ decrement } disabled={decBtnDisabled}>Decrease</button>
             </div>
+            <div>
+                <label>Category:</label>
+                <select>
+                    <option></option>
+                </select>
+            </div>
+            <div>
+                <button type="submit">Add Item</button>
+            </div>
+        </form>
 
     );
 
