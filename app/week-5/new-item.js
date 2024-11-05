581ed6b8c303cea3bc6daf0127fab2d26abb56d1
Taze-Priizer <diem.salaysay@edu.sait.ca>
2024-11-05 16:28:26 -0700

    Added and finished handleSubmit and onChange function

--- app/week-5/new-item.js
+++ app/week-5/new-item.js
@@ -24,21 +24,31 @@ export default function NewItem(){
             setQuantity(quantity - 1);
         }
     };
+
+    const handleNameChange = (event) => setName(event.target.value);
+    const handleQuantityChange = (event) => setQuantity(event.target.value);
+    const handleCategoryChange = (event) => setCategory(event.target.value);
     
     const handleSubmit = (event) => {
         event.preventDefault();
         console.dir(event);
 
-        const item = {
-            name,
-            category,
-            quantity
+        let itemObject = {
+            itemname: name,
+            itemcategory: category,
+            itemquantity: quantity
         }
         
-        console.log(item);
-        alert("Added name: " + name + " category: " + category + " quantity: " + quantity);
-        console.log(name, category, quantity);
+        alert(`
+            Name: ${itemObject.itemname}
+            Category: ${itemObject.itemcategory}
+            Quantity: ${itemObject.itemquantity}
+            `);
         reset();
+
+        setName("");
+        setCategory("");
+        setQuantity("");
     };
 
     const reset = () => {
@@ -52,24 +62,32 @@ export default function NewItem(){
     let btnStyles = "bg-blue-600 text-white rounded py-2 px-4 mt-5 mr-3 hover:bg-blue-400 active:bg-yellow-600 disabled:bg-red-400";
 
     return (
-        <form onSubmit={handleSubmit}>
-            <div>
-                <label>Name:</label>
-                <input type="text" />
+        <form onSubmit={handleSubmit} className="p-5 bg-orange-300">
+            <div className="mb-3">
+                <label className="inline-block w-40 align-top">Name:</label>
+                <input type="text"
+                onChange={handleNameChange}
+                value={name}
+                className="px-2 py-0.5 rounded bg-blue-100 focus:bg-green-200 border border-teal-600" 
+                />
             </div>
             <div className="bg-orange-300 p-5">
                 <p className="text-2xl lg">{quantity}</p>
                 <button className={btnStyles} onClick={ increment } disabled={incBtnDisabled}>Increase</button>
                 <button className={btnStyles} onClick={ decrement } disabled={decBtnDisabled}>Decrease</button>
             </div>
-            <div>
-                <label>Category:</label>
-                <select>
-                    <option></option>
+            <div className="mb-3">
+                <label className="inline-block w-40 align-top">Category:</label>
+                <select
+                onChange={handleCategoryChange}
+                value={category}
+                className="px-2 py-0.5 rounded bg-blue-100 focus:bg-green-200 border border-teal-600"
+                >
+                    <option>Category:</option>
                 </select>
             </div>
-            <div>
-                <button type="submit">Add Item</button>
+            <div className="mb-3"> 
+                <button type="submit" className="px-3 py-2 rounded text-white bg-green-600 hover:bg-green-500 active:bg-green-400">Add Item</button>
             </div>
         </form>
 
--- app/week-5/page.js
+++ app/week-5/page.js
@@ -1,9 +1,12 @@
 import NewItem from "./new-item";
 
+
 export default function Page(){
 
+    
+
     return(
-    <main className="p-5">
+    <main className="text-3xl text-center mb-3 p-5">
         <NewItem />
     </main>
     );
